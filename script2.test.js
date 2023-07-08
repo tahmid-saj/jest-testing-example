b9c867001c6b30faed51a5c79abbcd1324e591f1
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-08 18:58:24 -0400

    async mock tests

--- script2.test.js
+++ script2.test.js
@@ -2,12 +2,12 @@ const fetch = require("node-fetch");
 const swapi = require("./script2.js");
 
 it('calls swapi to get people', (done) => {
-  expect.assertions(1);
+  // expect.assertions(1);
 
   swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
-    
-    done();
+
+    // done();
   })
 });
 
@@ -18,6 +18,26 @@ it('calls swapi to get people with a promise', () => {
     expect(data.count).toEqual(87);
     expect(data.results.length).toBeGreaterThan(5);
 
-    done();
+    // done();
   });
+});
+
+it("getPeople returns count and results", () => {
+  const mockFetch = jest.fn()
+    .mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        count: 87,
+        results: [0, 1, 2, 3, 4, 5]
+      })
+    }))
+
+  expect.assertions(4);
+
+  return swapi.getPeoplePromise(mockFetch)
+    .then(data => {
+      expect(mockFetch.mock.calls.length).toBe(1);
+      expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
+      expect(data.count).toEqual(87);
+      expect(data.results.length).toBeGreaterThan(5);
+    });
 });
